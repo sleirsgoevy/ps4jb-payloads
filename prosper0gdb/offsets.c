8eb9d2ec09f1001ae1c9b7b9f77b9806358ebbc7
cheburek3000 <cheburek3000@protonmail.com>
2023-10-17 16:37:51 +0100

    Check and fix enough offsets for self dumper

--- prosper0gdb/offsets.c
+++ prosper0gdb/offsets.c
@@ -90,47 +90,47 @@ DEF(utoken, 0x6506500)
 END_FW()
 
 START_FW(450)
-DEF(allproc, 0x27edcb8)
-DEF(idt, 0x64cdc80)
-DEF(gdt_array, 0x64cee30)
-DEF(tss_array, 0x64d0830)
-DEF(pcpu_array, 0x64d2280)
-DEF(doreti_iret, -0x9cf84c)
-DEF(add_rsp_iret, doreti_iret - 7)
-DEF(swapgs_add_rsp_iret, doreti_iret - 10)
-DEF(rep_movsb_pop_rbp_ret, -0x99002a /*-0x990a55*/)
-DEF(rdmsr_start, -0x9d0cfa /*-0x9d6d02*/)
-//DEF(rdmsr_end, -0x9d6cf9)
-DEF(wrmsr_ret, -0x9d20cc)
-DEF(dr2gpr_start, -0x9d6d93)
-//DEF(dr2gpr_end, -0x9d6d7c)
-DEF(gpr2dr_1_start, -0x9d6c7a)
-//DEF(gpr2dr_1_end, -0x9d6c55)
-DEF(gpr2dr_2_start, -0x9d6b87)
-//DEF(gpr2dr_2_end, -0x9d6de9)
-DEF(mov_cr3_rax, -0x396f9e)
-DEF(mov_rdi_cr3, -0x39700e)
-DEF(nop_ret, -0x28a3a0)
-DEF(cpu_switch, -0x9d6f80)
-DEF(mprotect_fix_start, -0x90ac61)
-DEF(mprotect_fix_end, mprotect_fix_start+6)
-DEF(mmap_self_fix_1_start, -0x2cd31d)
-DEF(mmap_self_fix_1_end, mmap_self_fix_1_start+2)
-DEF(mmap_self_fix_2_start, -0x1df2ce)
-DEF(mmap_self_fix_2_end, mmap_self_fix_2_start+2)
+DEF(allproc, 0x27edcb8) // ok
+DEF(idt, 0x64cdc80) // ok
+DEF(gdt_array, 0x64cee30) // ok
+DEF(tss_array, 0x64d0830) // ok
+DEF(pcpu_array, 0x64d2280) // ok
+DEF(doreti_iret, -0x9cf84c) // ok
+DEF(add_rsp_iret, doreti_iret - 7) // ok
+DEF(swapgs_add_rsp_iret, doreti_iret - 10) // ok
+DEF(rep_movsb_pop_rbp_ret, -0x99002a /*-0x990a55*/) // ok
+DEF(rdmsr_start, -0x9d0cfa /*-0x9d6d02*/) // ok
+//DEF(rdmsr_end, -0x9d6cf9) // ok
+DEF(wrmsr_ret, -0x9d20cc) // ok
+DEF(dr2gpr_start, -0x9d6d93) // ok
+//DEF(dr2gpr_end, -0x9d6d7c) // ok
+DEF(gpr2dr_1_start, -0x9d6c7a) // ok
+//DEF(gpr2dr_1_end, -0x9d6c55) // ok
+DEF(gpr2dr_2_start, -0x9d6b87) // ok
+//DEF(gpr2dr_2_end, -0x9d6de9) // ok
+DEF(mov_cr3_rax, -0x396e4e) // ok?, fixed
+DEF(mov_rdi_cr3, -0x396ebe) // ok, fixed
+DEF(nop_ret, -0x396de1) // ok, fixed, got from pmap_activate_sw
+DEF(cpu_switch, -0x9d6f80) // ok
+DEF(mprotect_fix_start, -0x90ac61) // ok
+DEF(mprotect_fix_end, mprotect_fix_start+6) // ok
+DEF(mmap_self_fix_1_start, -0x2cd16d) // ok, fixed
+DEF(mmap_self_fix_1_end, mmap_self_fix_1_start+2) // ok
+DEF(mmap_self_fix_2_start, -0x1df11e) // ok, fixed
+DEF(mmap_self_fix_2_end, mmap_self_fix_2_start+2) // ok
 DEF(sigaction_fix_start, -0x6c2989)
 DEF(sigaction_fix_end, -0x6c2933)
-DEF(sysents, 0x1709c0)
-DEF(sysents_ps4, 0x168410)
-DEF(sysentvec, 0xd11bb8)
-DEF(sysentvec_ps4, 0xd11d30)
+DEF(sysents, 0x1709c0) // ok
+DEF(sysents_ps4, 0x168410) // ok
+DEF(sysentvec, 0xd11bb8) // ok
+DEF(sysentvec_ps4, 0xd11d30) // ok
 DEF(sceSblServiceMailbox, -0x6824c0)
 DEF(sceSblAuthMgrSmIsLoadable2, -0x8a5c40)
 DEF(mdbg_call_fix, -0x631ea9)
 DEF(syscall_before, -0x802311)
 DEF(syscall_after, -0x8022ee)
 DEF(malloc, -0xa9b00)
-DEF(M_something, 0x1346080)
+DEF(M_something, 0x1346080) // ok
 DEF(loadSelfSegment_epilogue, -0x8a54cd)
 DEF(loadSelfSegment_watchpoint, -0x2cc918)
 DEF(loadSelfSegment_watchpoint_lr, -0x8a5727)
@@ -154,18 +154,18 @@ DEF(sceSblServiceCryptAsync_deref_singleton, -0x8ed902)
 DEF(copyin, -0x9908e0)
 DEF(copyout, -0x990990)
 DEF(crypt_message_resolve, -0x479d60)
-DEF(justreturn, -0x9cf990)
-DEF(justreturn_pop, justreturn+8)
-DEF(mini_syscore_header, 0xdc16e8)
-DEF(pop_all_iret, -0x9cf8ab)
-DEF(pop_all_except_rdi_iret, pop_all_iret+4)
-DEF(push_pop_all_iret, -0x96be70)
-DEF(kernel_pmap_store, 0x3257a78)
-DEF(crypt_singleton_array, 0x2e31830)
-DEF(security_flags, 0x6506474)
-DEF(targetid, 0x650647d)
-DEF(qa_flags, 0x6506498)
-DEF(utoken, 0x6506500)
+DEF(justreturn, -0x9cf990) // ok
+DEF(justreturn_pop, justreturn+8) // ok
+DEF(mini_syscore_header, 0xdc16e8) // ok
+DEF(pop_all_iret, -0x9cf8ab) // ok
+DEF(pop_all_except_rdi_iret, pop_all_iret+4) // ok
+DEF(push_pop_all_iret, -0x96db88) // ok, fixed
+DEF(kernel_pmap_store, 0x3257a78) // ok
+DEF(crypt_singleton_array, 0x2e31830) // ok
+DEF(security_flags, 0x6506474) // ok
+DEF(targetid, 0x650647d) // ok
+DEF(qa_flags, 0x6506498) // ok
+DEF(utoken, 0x6506500) // ok
 #include "offset_list.txt"
 END_FW()
 
